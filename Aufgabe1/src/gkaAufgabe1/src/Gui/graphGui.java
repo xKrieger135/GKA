c88e9d87eb56e978b5031050387de3a635e71a9c
xKrieger135 <patrick.steinhauer@haw-hamburg.de>
2014-10-26 00:55:56 +0200

    Revert "Revert "Created Basics for GKA Task 1""
    This reverts commit 20533c2162fb6f186b35dfd989f4321090b2fb62.

---
+++ Aufgabe1/src/gkaAufgabe1/src/Gui/graphGui.java
@@ -0,0 +1,15 @@
+package Gui;
+
+import javax.swing.JFrame;
+
+public class graphGui {
+	
+	
+	
+	public static void main(String[] args) {
+		JFrame mainWindow = new JFrame("Gui für Graphen");
+		mainWindow.setSize(750, 500);
+		mainWindow.setVisible(true);
+	}
+
+}
