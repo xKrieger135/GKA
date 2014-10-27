3f1b435335564e3662a8f176c7255c509ee01827
xKrieger135 <patrick.steinhauer@haw-hamburg.de>
2014-10-27 16:49:15 +0100

    New Changes

--- Aufgabe1/src/gkaAufgabe1/src/Gui/graphGui.java
+++ Aufgabe1/src/gkaAufgabe1/src/Gui/graphGui.java
@@ -8,7 +8,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 import org.jgraph.JGraph;
 import org.jgraph.graph.DefaultEdge;
@@ -19,7 +18,7 @@ import org.jgrapht.ext.JGraphModelAdapter;
 import org.jgrapht.graph.ListenableDirectedGraph;
 
 public class graphGui extends JFrame {
-	private JGraphModelAdapter jGraphModelAdapter;
+	private JGraphModelAdapter<String, DefaultEdge> jGraphModelAdapter;
 	private JGraph jGraph;
 
 	private static final Color DEFAULT_BG_COLOR = Color.decode("#c7d2ff");
@@ -35,22 +34,18 @@ public class graphGui extends JFrame {
 		mainWindow.setVisible(true);
 		mainWindow.setDefaultCloseOperation(mainWindow.EXIT_ON_CLOSE);
 
-		JPanel graphWindow = new JPanel();
-		graphWindow.setSize(150, 150);
-		graphWindow.setVisible(true);
-		mainWindow.getContentPane().add(graphWindow);
-
 		// create a JGraphT graph
-		ListenableGraph g = new ListenableDirectedGraph(DefaultEdge.class);
+		ListenableGraph<String, DefaultEdge> g = new ListenableDirectedGraph<String, DefaultEdge>(
+				DefaultEdge.class);
 
 		// create a visualization using JGraph, via an adapter
-		jGraphModelAdapter = new JGraphModelAdapter(g);
+		jGraphModelAdapter = new JGraphModelAdapter<String, DefaultEdge>(g);
 
 		jGraph = new JGraph(jGraphModelAdapter);
 
 		graphColorChanges(jGraph);
-		graphWindow.add(jGraph);
-		resize(DEFAULT_SIZE);
+		mainWindow.getContentPane().add(jGraph);
+		setSize(DEFAULT_SIZE);
 
 		// add some sample data (graph manipulated via JGraphT)
 		g.addVertex("v1");
@@ -60,14 +55,14 @@ public class graphGui extends JFrame {
 
 		g.addEdge("v1", "v2");
 		g.addEdge("v2", "v3");
-		g.addEdge("v3", "v1");
+		g.addEdge("v1", "v4");
 		g.addEdge("v4", "v3");
 
 		// position vertices nicely within JGraph component
 		positionVertexAt("v1", 130, 40);
 		positionVertexAt("v2", 60, 200);
 		positionVertexAt("v3", 310, 230);
-		positionVertexAt("v4", 380, 70);
+		positionVertexAt("v4", 300, 70);
 
 		// that's all there is to it!...
 	}
