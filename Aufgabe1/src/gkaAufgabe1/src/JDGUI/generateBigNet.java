f965698d665379a214999e1e256456c7c1f75769
Blackgen <jandb07@gmx.de>
2014-12-30 23:02:19 +0100

    Updated generator

--- Aufgabe1/src/gkaAufgabe1/src/JDGUI/generateBigNet.java
+++ Aufgabe1/src/gkaAufgabe1/src/JDGUI/generateBigNet.java
@@ -17,6 +17,8 @@ public class generateBigNet {
 	private int VertexCount =800;
 	private int VertexRange=VertexCount/100 * RandomRangeInPercent;
 	
+	private int maxGewicht= 100;
+	
 	
 	public Graph generateBigNet() {
 		Graph<String, WeightedEdge> Out = new DirectedWeightedPseudograph<>(
@@ -36,7 +38,41 @@ public class generateBigNet {
 		}
 		
 		
-		return null;
+		int edges =0;
+		// Von s zu vx
+		int random=random(1,10);
+		for (int i = 0; i < random; i++) {
+			WeightedEdge e = new WeightedEdge();
+			e.setWeight(random(0,maxGewicht));
+			Out.addEdge("s", "v"+random(1,EdgeCount),e);
+			edges+=1;
+		}
+		
+		// von s zu q
+		random=random(0,1);
+		for (int i = 0; i < random; i++) {
+			WeightedEdge e = new WeightedEdge();
+			e.setWeight(random(0,maxGewicht));
+			Out.addEdge("s", "q",e);
+			edges+=1;
+		}
+		
+		// von vx zu q
+		random=random(1,10);
+		for (int i = 0; i < random; i++) {
+			WeightedEdge e = new WeightedEdge();
+			e.setWeight(random(0,maxGewicht));
+			Out.addEdge("v"+random(1,EdgeCount),"q",e);
+			edges+=1;
+		}
+		
+		
+		for (int i = 0; i < remainingEdges-edges; i++) {
+			WeightedEdge e = new WeightedEdge();
+			e.setWeight(random(0,maxGewicht));
+			Out.addEdge("v"+random(1,EdgeCount), "v"+random(1,EdgeCount),e);
+		}
+		return Out;
 		
 	}
 	private static int random(int min, int max)
